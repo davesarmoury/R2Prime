d469620be27a0bc2714562e0d98941d41c8210dd
Dave Niewinski <davesarmoury@gmail.com>
2026-03-20 10:02:58 -0400

    Added linear actuator for the center leg.  Updated static script to use it

--- r2_control/scripts/static_323.py
+++ r2_control/scripts/static_323.py
@@ -4,16 +4,16 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 
-names = ["ankle_joint", "shoulder_joint", "center_ankle_joint"]
-two = [0.0, 0.0, 0.0]
-three = [0.3054326, -0.6108652, 0.3054326]
+names = ["ankle_joint", "shoulder_joint", "center_ankle_joint", "center_leg_joint"]
+two = [0.0, 0.0, 0.0, 0.255]
+three = [0.3054326, -0.6108652, 0.3054326, 0.0]
 values = three
 
 class StaticR2JointPublisher(Node):
     def __init__(self):
         super().__init__('static_r2_joint_publisher')
         self.publisher = self.create_publisher(JointState, 'joint_states', 10)
-        self.timer = self.create_timer(0.1, self.timer_callback)  # 10 Hz
+        self.timer = self.create_timer(0.05, self.timer_callback)  # 20 Hz
 
     def timer_callback(self):
         msg = JointState()
